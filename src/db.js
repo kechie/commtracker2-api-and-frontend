48b28e923c10773379a99f072708e369496c2ac3
Richard L. Alhama <richard.alhama@gmail.com>
2026-01-23 13:05:47 +0800

    ok tito no problem

--- src/db.js
+++ src/db.js
@@ -4,7 +4,6 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
