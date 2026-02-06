d082ae698858a8fde704befeda7c0e5b7c626745
Richard L. Alhama <richard.alhama@gmail.com>
2026-02-06 21:37:15 +0800

    ok tito

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,28 +3,20 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    dedupe: ['react', 'react-dom'],
+  },
   build: {
-    chunkSizeWarningLimit: 1600,
-    rollupOptions: {
-      output: {
-      manualChunks: undefined, // Remove manualChunks
-      advancedChunks: {
-        groups: [{ name: 'vendor', test:  /[\\/]node_modules[\\/](react|react-dom)[\\/]/ }]
-          }
-      }
-    }
+    chunkSizeWarningLimit: 2000,
   },
   optimizeDeps: {
+    include: ['recharts'],
     rolldownOptions: {
       jsx: 'automatic',
     },
   },
   plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
+    react(),
     {
       name: 'silence-rolldown-warning',
       enforce: 'post',
