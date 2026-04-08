e8109badf70e596877c4b4518c61727f8edcf05a
Richard Alhama <richard.alhama@gmail.com>
2026-04-08 09:10:33 +0800

    module upgrades

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: ['dir.laoagcity.gov.ph']
+  },
   build: {
     chunkSizeWarningLimit: 1600,
     rollupOptions: {
